2a4b19323539b36204fd3d0d0dcef925067ace7c
Sorawit Trutsat (Kong) <sorawit.tr@gmail.com>
2022-08-10 20:12:52 +0700

    first commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sorawit Trutsat on 10/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
